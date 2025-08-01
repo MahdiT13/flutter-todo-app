7435decc0510d4f5b539db54a0d366291a9152f5
Mt0o013 <hawksa@gmail.com>
2025-08-01 19:25:43 +0300

    initial commit

--- lib/Screens/add_task_screen.dart
+++ lib/Screens/add_task_screen.dart
@@ -48,105 +48,110 @@ class AddTaskScreenState extends State<AddTaskScreen> {
         iconTheme: IconThemeData(color: Colors.white),
         backgroundColor: Color(0xff5faeb6),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: ListView(
-          children: [
-            TextField(
-              controller: _titleController,
-              decoration: InputDecoration(labelText: 'Task Title'),
-            ),
-            SizedBox(height: 12),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: TaskCategory.values.map((cat) {
-                IconData icon;
-                switch (cat) {
-                  case TaskCategory.todo:
-                    icon = Icons.book;
-                    break;
-                  case TaskCategory.workout:
-                    icon = Icons.fitness_center;
-                    break;
-                  case TaskCategory.event:
-                    icon = Icons.celebration;
-                    break;
-                }
-                return IconButton(
-                  icon: Icon(
-                    icon,
-                    color: _selectedCategory == cat
-                        ? Colors.black
-                        : Colors.white,
-                  ),
-                  onPressed: () => setState(() => _selectedCategory = cat),
-                );
-              }).toList(),
-            ),
-            ListTile(
-              title: Text(
-                'Date: ${_selectedDate.toLocal().toString().split(' ')[0]}',
-                style: GoogleFonts.caudex(fontSize: 20),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: ListView(
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(labelText: 'Task Title'),
               ),
-              trailing: Icon(Icons.calendar_today, color: Colors.black),
-              onTap: () async {
-                final date = await showDatePicker(
-                  context: context,
-                  initialDate: _selectedDate,
-                  firstDate: DateTime(2020),
-                  lastDate: DateTime(2100),
-                );
-                if (date != null) setState(() => _selectedDate = date);
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Time: ${_selectedTime.format(context)}',
-                style: GoogleFonts.caudex(fontSize: 20),
+              SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: TaskCategory.values.map((cat) {
+                  IconData icon;
+                  switch (cat) {
+                    case TaskCategory.todo:
+                      icon = Icons.book;
+                      break;
+                    case TaskCategory.workout:
+                      icon = Icons.fitness_center;
+                      break;
+                    case TaskCategory.event:
+                      icon = Icons.celebration;
+                      break;
+                  }
+                  return IconButton(
+                    icon: Icon(
+                      icon,
+                      color: _selectedCategory == cat
+                          ? Colors.black
+                          : Colors.white,
+                    ),
+                    onPressed: () => setState(() => _selectedCategory = cat),
+                  );
+                }).toList(),
               ),
-              trailing: Icon(Icons.access_time, color: Colors.black),
-              onTap: () async {
-                final time = await showTimePicker(
-                  context: context,
-                  initialTime: _selectedTime,
-                );
-                if (time != null) setState(() => _selectedTime = time);
-              },
-            ),
-            TextField(
-              controller: _notesController,
-              maxLines: 3,
-              decoration: InputDecoration(labelText: 'Notes'),
-            ),
-            SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(50)),
-                border: Border(
-                  top: BorderSide(color: Color(0xff323a45), width: 1),
-                  bottom: BorderSide(color: Color(0xff323a45), width: 5),
-                  left: BorderSide(color: Color(0xff323a45), width: 2),
-                  right: BorderSide(color: Color(0xff323a45), width: 2),
+              ListTile(
+                title: Text(
+                  'Date: ${_selectedDate.toLocal().toString().split(' ')[0]}',
+                  style: GoogleFonts.caudex(fontSize: 20),
                 ),
+                trailing: Icon(Icons.calendar_today, color: Colors.black),
+                onTap: () async {
+                  final date = await showDatePicker(
+                    context: context,
+                    initialDate: _selectedDate,
+                    firstDate: DateTime(2020),
+                    lastDate: DateTime(2100),
+                  );
+                  if (date != null) setState(() => _selectedDate = date);
+                },
               ),
-              child: ElevatedButton(
-                onPressed: _saveTask,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xff3f6184),
-                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+              ListTile(
+                title: Text(
+                  'Time: ${_selectedTime.format(context)}',
+                  style: GoogleFonts.caudex(fontSize: 20),
                 ),
-                child: Text(
-                  'Save',
-                  style: GoogleFonts.caudex(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    letterSpacing: 6,
+                trailing: Icon(Icons.access_time, color: Colors.black),
+                onTap: () async {
+                  final time = await showTimePicker(
+                    context: context,
+                    initialTime: _selectedTime,
+                  );
+                  if (time != null) setState(() => _selectedTime = time);
+                },
+              ),
+              TextField(
+                controller: _notesController,
+                maxLines: 3,
+                decoration: InputDecoration(labelText: 'Notes'),
+              ),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  border: Border(
+                    top: BorderSide(color: Color(0xff323a45), width: 1),
+                    bottom: BorderSide(color: Color(0xff323a45), width: 5),
+                    left: BorderSide(color: Color(0xff323a45), width: 2),
+                    right: BorderSide(color: Color(0xff323a45), width: 2),
                   ),
                 ),
+                child: ElevatedButton(
+                  onPressed: _saveTask,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff3f6184),
+                    padding: EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.height * 0.02,
+                      horizontal: MediaQuery.of(context).size.width * 0.06,
+                    ),
+                  ),
+                  child: Text(
+                    'Save',
+                    style: GoogleFonts.caudex(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      letterSpacing: 6,
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -6,7 +6,7 @@ import 'add_task_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
 
   @override
   HomeScreenState createState() => HomeScreenState();
@@ -65,7 +65,7 @@ class HomeScreenState extends State<HomeScreen> {
     final pending = tasks.where((t) => !t.isDone).toList();
 
     return Scaffold(
-      backgroundColor: const Color(0xff3f6184),
+      backgroundColor: Color(0xff3f6184),
       appBar: AppBar(
         title: Text(
           'My Todo List',
@@ -76,146 +76,148 @@ class HomeScreenState extends State<HomeScreen> {
           ),
         ),
         centerTitle: true,
-        backgroundColor: const Color(0xff3f6184),
+        backgroundColor: Color(0xff3f6184),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Container(
-          height: 700,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: const Color(0xff5faeb6),
-            border: Border.all(color: const Color(0xff323a45), width: 6),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.05,
           ),
-          child: Column(
-            children: [
-              const SizedBox(height: 10),
-              Text(
-                "Today - ${DateTime.now().toLocal().toString().split(' ')[0]}",
-                style: GoogleFonts.caudex(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.85,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Color(0xff5faeb6),
+              border: Border.all(
+                color: Color(0xff323a45),
+                width: MediaQuery.of(context).size.width * 0.02,
               ),
-              const Divider(color: Colors.black, indent: 55, endIndent: 50),
-              Expanded(
-                child: ListView(
-                  children: [
-                    ...pending.map(
-                      (task) => Dismissible(
-                        key: ValueKey(task.id),
-                        crossAxisEndOffset: -1,
-                        direction: DismissDirection.endToStart,
-                        background: Container(
-                          alignment: Alignment.centerRight,
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          color: Colors.red,
-                          child: const Icon(
-                            Icons.delete,
-                            color: Colors.white,
-                            size: 30,
-                          ),
-                        ),
-                        onDismissed: (_) {
-                          _deleteTask(task);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('Task deleted')),
-                          );
-                        },
-                        child: Column(
-                          children: [
-                            TaskCard(
-                              task: task,
-                              onToggle: () => _toggleDone(task),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    if (completed.isNotEmpty) ...[
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Completed (Swipe to delete)',
-                          style: GoogleFonts.caudex(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      ...completed.map(
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: 10),
+                Text(
+                  "Today - ${DateTime.now().toLocal().toString().split(' ')[0]}",
+                  style: GoogleFonts.caudex(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Divider(color: Colors.black, indent: 55, endIndent: 50),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      ...pending.map(
                         (task) => Dismissible(
                           key: ValueKey(task.id),
-                          direction: DismissDirection.endToStart,
                           crossAxisEndOffset: -1,
+                          direction: DismissDirection.startToEnd,
                           background: Container(
-                            alignment: Alignment.centerRight,
-                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            alignment: Alignment.centerLeft,
+                            padding: EdgeInsets.symmetric(horizontal: 20),
                             color: Colors.red,
-                            child: const Icon(
+                            child: Icon(
                               Icons.delete,
                               color: Colors.white,
+                              size: 30,
                             ),
                           ),
                           onDismissed: (_) {
                             _deleteTask(task);
                             ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Task deleted')),
+                              SnackBar(content: Text('Task deleted')),
                             );
                           },
-                          child: TaskCard(
-                            task: task,
-                            onToggle: () => _toggleDone(task),
+                          child: Column(
+                            children: [
+                              TaskCard(
+                                task: task,
+                                onToggle: () => _toggleDone(task),
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                    ],
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    border: const Border(
-                      top: BorderSide(color: Color(0xff323a45), width: 1),
-                      bottom: BorderSide(color: Color(0xff323a45), width: 5),
-                      left: BorderSide(color: Color(0xff323a45), width: 2),
-                      right: BorderSide(color: Color(0xff323a45), width: 2),
-                    ),
-                  ),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xff3f6184),
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 15,
-                        horizontal: 50,
-                      ),
-                    ),
-                    child: Text(
-                      'Add New Task',
-                      style: GoogleFonts.caudex(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        letterSpacing: 6,
-                      ),
-                    ),
-                    onPressed: () async {
-                      final result = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => const AddTaskScreen(),
+                      if (completed.isNotEmpty) ...[
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'Completed (Swipe to delete)',
+                            style: GoogleFonts.caudex(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
-                      );
-                      if (result != null && result is Task) {
-                        _addTask(result);
-                      }
-                    },
+                        ...completed.map(
+                          (task) => Dismissible(
+                            key: ValueKey(task.id),
+                            direction: DismissDirection.startToEnd,
+                            crossAxisEndOffset: -1,
+                            background: Container(
+                              alignment: Alignment.centerLeft,
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              color: Colors.red,
+                              child: Icon(Icons.delete, color: Colors.white),
+                            ),
+                            onDismissed: (_) {
+                              _deleteTask(task);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('Task deleted')),
+                              );
+                            },
+                            child: TaskCard(
+                              task: task,
+                              onToggle: () => _toggleDone(task),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ],
                   ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border(
+                        top: BorderSide(color: Color(0xff323a45), width: 1),
+                        bottom: BorderSide(color: Color(0xff323a45), width: 5),
+                        left: BorderSide(color: Color(0xff323a45), width: 2),
+                        right: BorderSide(color: Color(0xff323a45), width: 2),
+                      ),
+                    ),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff3f6184),
+                        padding: EdgeInsets.symmetric(
+                          vertical: MediaQuery.of(context).size.height * 0.02,
+                          horizontal: MediaQuery.of(context).size.width * 0.07,
+                        ),
+                      ),
+                      child: Text(
+                        'Add New Task',
+                        style: GoogleFonts.caudex(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          letterSpacing: 2,
+                        ),
+                      ),
+                      onPressed: () async {
+                        final result = await Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (_) => AddTaskScreen()),
+                        );
+                        if (result != null && result is Task) {
+                          _addTask(result);
+                        }
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
